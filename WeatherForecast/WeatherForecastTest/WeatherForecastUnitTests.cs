6756658fd85a5fb2b01749a16ab6e608bed15009
Petter Astrup <petter.astrup@emmasys.com>
2025-11-12 20:48:28 +0100

    remove test

--- WeatherForecast/WeatherForecastTest/WeatherForecastUnitTests.cs
+++ WeatherForecast/WeatherForecastTest/WeatherForecastUnitTests.cs
@@ -4,16 +4,6 @@ namespace WeatherApi.Tests;
 
 public class WeatherForecastUnitTests
 {
-    [Theory]
-    [InlineData(-20, -4)]
-    [InlineData(0, 32)]
-    [InlineData(10, 49)]
-    [InlineData(25, 76)]
-    public void TemperatureF_IsCalculatedFromCelsius(int c, int expectedF)
-    {
-        var wf = new WeatherForecast(DateOnly.FromDateTime(DateTime.UtcNow), c, "Mild");
-        wf.TemperatureF.Should().Be(expectedF);
-    }
 
 
     [Fact]
