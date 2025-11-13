d2903dd1877d79ff3157e9aaaf47cbd6e9324f48
Petter Astrup <petter.astrup@emmasys.com>
2025-11-13 07:32:13 +0100

    Add strange code

--- WeatherForecast/WeatherForecast/Program.cs
+++ WeatherForecast/WeatherForecast/Program.cs
@@ -16,25 +16,37 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+    "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
+
+// Kan typisk trigge Roslynator (“kan være const”)
+var days = 5;
 
 app.MapGet("/weatherforecast", () =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
+        var start = DateTime.Now;
+
+        var forecast = Enumerable
+            .Range(1, days)
+            .Select(index =>
+            {
+                var date = DateOnly.FromDateTime(start.AddDays(index));
+                var temperatureC = Random.Shared.Next(-20, 55);
+
+                var summaryIndex = Random.Shared.Next(summaries.Length);
+                var summary = summaries[summaryIndex];
+
+                return new WeatherForecast(date, temperatureC, summary);
+            })
+            .ToArray()
             .ToArray();
+
         return forecast;
     })
     .WithName("GetWeatherForecast");
 
 app.Run();
-
 
 public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
